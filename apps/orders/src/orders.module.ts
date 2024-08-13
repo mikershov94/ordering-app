4b3cc20d67facafaf681462fd5b0c627e513e4aa
Michael E. Ershov <em@newton.finance>
2024-08-13 12:56:31 +0800

    Updated module

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -1,9 +1,12 @@
+import * as Joi from 'joi';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import * as Joi from 'joi';
+import { MongooseModule } from '@nestjs/mongoose';
 import { DatabaseModule } from '@app/common';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
+import { OrdersRepository } from './orders.repository';
+import { Order, OrderSchema } from './schemas/orders.schema';
 
 @Module({
   imports: [
@@ -15,8 +18,9 @@ import { OrdersService } from './orders.service';
       envFilePath: './apps/orders/.env',
     }),
     DatabaseModule,
+    MongooseModule.forFeature([{name: Order.name, schema: OrderSchema}])
   ],
   controllers: [OrdersController],
-  providers: [OrdersService],
+  providers: [OrdersService, OrdersRepository],
 })
 export class OrdersModule {}
