68e926c418000a992d2052ece9bc2ff97190e948
Michael E. Ershov <em@newton.finance>
2024-08-23 11:36:16 +0800

    Added service-method createOrder

--- apps/orders/src/orders.controller.ts
+++ apps/orders/src/orders.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { OrdersService } from './orders.service';
+import { CreateOrderRequest } from './dto/create-order.request';
 
 @Controller()
 export class OrdersController {
