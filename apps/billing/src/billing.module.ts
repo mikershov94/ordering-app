e2e0788a039ec8a11a325166b7ee5eefcead9a6f
Michael E. Ershov <em.ershov@yandex.ru>
2024-08-27 12:38:09 +0800

    Added envs and rmq connect

--- apps/billing/src/billing.module.ts
+++ apps/billing/src/billing.module.ts
@@ -1,10 +1,21 @@
+import Joi from 'joi';
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { RmqModule } from '@app/common';
 import { BillingController } from './billing.controller';
 import { BillingService } from './billing.service';
 
 @Module({
-  imports: [RmqModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        RABBIT_MQ_URI: Joi.string().required(),
+        RABBIT_MQ_BILLING_QUEUE: Joi.string().required(),
+      }),
+    }),
+    RmqModule,
+  ],
   controllers: [BillingController],
   providers: [BillingService],
 })
